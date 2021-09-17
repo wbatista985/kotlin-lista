210c556d0cae580f2d6e4928ae0b98108ce2d842
FIC <wbatista985@gmail.com>
2021-09-17 20:44:06 -0300

    final projeto view
    final projeto rolando view

---
+++ app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
@@ -0,0 +1,46 @@
+package com.example.affirmations.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.affirmations.R
+import com.example.affirmations.model.Affirmation
+
+
+  //Adapter for the [RecyclerView] in [MainActivity]. Displays [Affirmation] data object.
+
+class ItemAdapter(
+    private val context: Context,
+    private val dataset: List<Affirmation>
+) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+
+    class ItemViewHolder(private val view: View): RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.item_title)
+        val imageView: ImageView = view.findViewById(R.id.item_image)
+    }
+    //Create new views (invoked by the layout manager)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        // create a new view
+        val adapterLayout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item, parent, false)
+
+        return ItemViewHolder(adapterLayout)
+    }
+
+
+     // Replace the contents of a view (invoked by the layout manager)
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val item = dataset[position]
+        holder.textView.text = context.resources.getString(item.stringResourceId)
+        holder.imageView.setImageResource(item.imageResourceId)
+    }
+
+    override fun getItemCount() = dataset.size
+
+}
